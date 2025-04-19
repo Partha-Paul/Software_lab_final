708e91d142de6770112208fe90b892003177288d
Partha Paul <parthapaul01709@gmail.com>
2025-04-19 09:03:47 +0000

    bug solved

--- test/test.cpp
+++ test/test.cpp
@@ -5,7 +5,7 @@ TEST(VehicleFactoryTest, CarDrive) {
     auto car = VehicleFactory::createVehicle(VehicleType::CAR);
     EXPECT_EQ(car->drive(), "Driving a car");
 }
-
+//bug solved
 TEST(VehicleFactoryTest, BikeDrive) {
     auto bike = VehicleFactory::createVehicle(VehicleType::BIKE);
     EXPECT_EQ(bike->drive(), "Riding a bike");
